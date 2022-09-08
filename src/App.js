921e161003041d8a9b70ca66057c6477c145d1a4
farrel2112 <farrelbaty87@gmail.com>
2022-09-08 13:36:43 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component }  from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className = "App" >
+            <h1> Gestionnaire des tâches</h1> 
+        </div>
+    );
+}
+
+export default App;
+
